6b61c4bc084d077a577935f47974c44d8a284a68
May Lee <mbenz10120112@gmail.com>
2022-07-06 09:15:36 -0400

    finished

---
+++ src/Lottery.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Wheel } from 'react-custom-roulette'
+
+const data = [
+  { option: '0', style: { backgroundColor: 'green', textColor: 'black' } },
+  { option: '1', style: { backgroundColor: 'white' } },
+  { option: '2' },
+];
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default ({mustSpin}) => (
+  <>
+    <Wheel
+      mustStartSpinning={mustSpin}
+      prizeNumber={3}
+      data={data}
+      backgroundColors={['#3e3e3e', '#df3428']}
+      textColors={['#ffffff']}
+    />
+  </>
+)
